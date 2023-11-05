f31f2dd4771fc910b094dd733d30b445f30d3048
Roumcha <68161142+roumcha@users.noreply.github.com>
2023-11-05 23:13:37 +0900

    [Hello, CDK!]

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import * as cdk from "aws-cdk-lib";
+import { App } from "aws-cdk-lib";
 import { CdkWorkshopStack } from "../lib/cdk-workshop-stack";
 
-const app = new cdk.App();
+const app = new App();
 new CdkWorkshopStack(app, "CdkWorkshopStack");
