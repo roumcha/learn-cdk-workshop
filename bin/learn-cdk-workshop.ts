8d38ae079425fbcfd3551cdacd77cc7506f022b4
Roumcha <68161142+roumcha@users.noreply.github.com>
2023-11-05 13:40:15 +0900

    remove "learn" prefix

--- bin/learn-cdk-workshop.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { LearnCdkWorkshopStack } from '../lib/learn-cdk-workshop-stack';
-
-const app = new cdk.App();
-new LearnCdkWorkshopStack(app, 'LearnCdkWorkshopStack');
