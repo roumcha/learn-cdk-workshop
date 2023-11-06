2ae541990df642fba35c9b3057a0e8bc277be95f
Roumcha <68161142+roumcha@users.noreply.github.com>
2023-11-06 15:22:51 +0900

    _

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -2,8 +2,6 @@ import * as cdk from "aws-cdk-lib";
 import * as codecommit from "aws-cdk-lib/aws-codecommit";
 import { Construct } from "constructs";
 
-// test commit
-
 export class WorkshopPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
