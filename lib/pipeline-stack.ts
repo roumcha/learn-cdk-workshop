402e1ef876dd2cb4401075f9744be48be9aa31fb
Roumcha <68161142+roumcha@users.noreply.github.com>
2023-11-06 16:21:46 +0900

    pnpm with sh

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -21,8 +21,9 @@ export class WorkshopPipelineStack extends cdk.Stack {
       pipelineName: "WorkshopPipeline",
       synth: new CodeBuildStep("SynthStep", {
         input: CodePipelineSource.codeCommit(repo, "main"),
-        installCommands: ["npm i -g aws-cdk"],
-        commands: ["rm pnpm-lock.yaml", "npm install", "npx cdk synth"],
+        installCommands: ["npm i -g pnpm", "pnpm setup", "pnpm i"],
+        commands: ["pnpm build", "pnpm dlx cdk synth"],
+        env: { SHELL: "sh" },
       }),
     });
   }
